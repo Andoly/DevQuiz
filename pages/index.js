74239d83607fa6788649a48acf588e476334fc68
Andoly <andoly.cs@hotmail.com>
2021-02-01 14:40:52 -0300

    Punctuation

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
-import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GithubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
